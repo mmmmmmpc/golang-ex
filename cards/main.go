b3c5e940653f726f3a0be81d4614aca51bcbd7f9
Miguel Pérez Colino <miguelp@vmware.com>
2022-03-21 09:22:04 +0100

    Var definition

--- cards/main.go
+++ cards/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of spades"
+	//var card string = "Ace of spades"
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
