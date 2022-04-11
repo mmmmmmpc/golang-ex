62ab6c742614c79ab5257f67468778714a6400ce
Miguel Pérez Colino <miguelp@vmware.com>
2022-04-11 15:47:33 +0200

    Added Deck function.

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of diamonds", newCard()}
+	cards := deck{"Ace of diamonds", newCard()}
 	cards = append(cards, "Six of spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
